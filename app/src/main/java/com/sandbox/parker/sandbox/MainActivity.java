925282e893a575ca9e9d6af638eea058c829110f
Parker Hughes <phughes@alwayseducation.com>
2017-09-04 11:58:09 -0400

    Added JODA time library

--- app/src/main/java/com/sandbox/parker/sandbox/MainActivity.java
+++ app/src/main/java/com/sandbox/parker/sandbox/MainActivity.java
@@ -2,6 +2,9 @@ package com.sandbox.parker.sandbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.joda.time.DateTime;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DateTime time = new DateTime(DateTime.now());
+        TextView timeView = ((TextView) findViewById(R.id.time_view));
+        timeView.setText(time.toString());
     }
 }
