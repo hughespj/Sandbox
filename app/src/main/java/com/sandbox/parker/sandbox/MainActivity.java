fca16e1d95b9098152719f48fd050aac4bfddf6e
Parker Hughes <phughes@alwayseducation.com>
2017-09-04 11:34:43 -0400

    Initialize Commit

---
+++ app/src/main/java/com/sandbox/parker/sandbox/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sandbox.parker.sandbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
