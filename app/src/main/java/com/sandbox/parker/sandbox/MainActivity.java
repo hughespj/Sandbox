1c493ac0b408fa9d17971d8244ca83b0032f4121
Parker Hughes <phughes@alwayseducation.com>
2017-09-05 15:20:24 -0400

    Added Nav Drawer and profiles

--- app/src/main/java/com/sandbox/parker/sandbox/MainActivity.java
+++ app/src/main/java/com/sandbox/parker/sandbox/MainActivity.java
@@ -1,11 +1,11 @@
 package com.sandbox.parker.sandbox;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,7 +16,9 @@ import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
 import com.mikepenz.materialdrawer.util.DrawerImageLoader;
 import com.squareup.picasso.Picasso;
@@ -30,6 +32,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private SharedPreferences navPrefs;
+    private IProfile savedProfile;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +52,18 @@ public class MainActivity extends AppCompatActivity {
         TextView timeView = ((TextView) findViewById(R.id.time_view));
         timeView.setText(time.toString());
 
+
+
         initializeNavDrawer();
     }
 
     private void initializeNavDrawer() {
+
+        navPrefs = this.getSharedPreferences("com.sandbox.parker.sandbox", Context.MODE_PRIVATE);
+
+
         DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+
             @Override
             public void set(ImageView imageView, Uri uri, Drawable placeholder) {
                 Picasso
@@ -67,9 +81,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        AccountHeader accountHeader = new AccountHeaderBuilder()
+        List<IProfile> profileList = new ArrayList<IProfile>();
+        String[] profileNames = getResources().getStringArray(R.array.profile_values);
+
+        for (String name : profileNames) {
+            IProfile profile = new ProfileDrawerItem();
+            profile.withName(name);
+            profileList.add(profile);
+        }
+
+        final AccountHeader accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
+                .withHeaderBackground(R.drawable.navdrawer_background)
+                .withProfiles(profileList)
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        setSavedProfile(profile);
+                        return true;
+                    }
+                })
                 .build();
+
+        accountHeader.setActiveProfile(getSavedProfile());
+
+//        for (IProfile profile : profileList) {
+//            if (profile.getName().toString() == navPrefs.getString("name", null)) {
+//                accountHeader.setActiveProfile(profile);
+//            }
+//
+//        }
 
         Drawer drawer = new DrawerBuilder()
                 .withActivity(this)
@@ -83,6 +124,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
                 .build();
+
+
     }
 
     private List<IDrawerItem> getDrawerItems() {
@@ -106,5 +149,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return drawerItems;
+    }
+
+    @Override
+    protected void onDestroy() {
+        navPrefs.edit().putString("name", getSavedProfile().getName().toString());
+        navPrefs.edit().apply();
+        super.onDestroy();
+    }
+
+    public IProfile getSavedProfile() {
+        return savedProfile;
+    }
+
+    public void setSavedProfile(IProfile savedProfile) {
+        this.savedProfile = savedProfile;
     }
 }
